7f11e9fa4c8c82298b96d3cb5d3ac8983cbda945
xycarto <xycarto@gmail.com>
2022-05-04 18:09:43 +1200

    change value in selection start

--- main.js
+++ main.js
@@ -7,40 +7,40 @@ import * as olProj from 'ol/proj';
 import GeoTIFF from 'ol/source/GeoTIFF';
 import Overlay from 'ol/Overlay';
 
-//toggle seafloor view 
-document.getElementById("menu-ui").onclick = function() { 
-  cog.setVisible(!cog.getVisible());
-}; 
+// //toggle seafloor view
+// document.getElementById("menu-ui").onclick = function() {
+//   cog.setVisible(!cog.getVisible());
+// };
 
-const container = document.getElementById('popup');
-const content = document.getElementById('popup-content');
-const closer = document.getElementById('popup-closer');
+// const container = document.getElementById('popup');
+// const content = document.getElementById('popup-content');
+// const closer = document.getElementById('popup-closer');
 
 // Create an overlay to anchor the popup to the map
- const overlay = new Overlay({
-  element: container,
-  autoPan: {
-    animation: {
-      duration: 250,
-    },
-  },
-});
+//  const overlay = new Overlay({
+//   element: container,
+//   autoPan: {
+//     animation: {
+//       duration: 250,
+//     },
+//   },
+// });
 
-/**
- * Add a click handler to hide the popup.
- * @return {boolean} Don't follow the href.
- */
-closer.onclick = function () {
-  overlay.setPosition(undefined);
-  closer.blur();
-  return false;
-};
+// /**
+//  * Add a click handler to hide the popup.
+//  * @return {boolean} Don't follow the href.
+//  */
+// closer.onclick = function () {
+//   overlay.setPosition(undefined);
+//   closer.blur();
+//   return false;
+// };
 
 // URL to COG tile
 const url = 'https://tile-service-raster.s3.us-east-1.amazonaws.com/cogs/seafloor/HS51_Seafloor_Classification_webmer_cog.tif'
 
 // Grab COG tile, set min, max, nodata
-const source = new GeoTIFF({
+const cogSource = new GeoTIFF({
   normalize: false,
   sources: [
     {
@@ -52,12 +52,83 @@ const source = new GeoTIFF({
   ],
 });
 
+const cogVis = [
+  {
+    name: "All Values",
+    sources: [
+      {
+        min: 1,
+        max: 4,
+        nodata: 127,
+      }
+    ],
+    style: {
+      color: [
+        'interpolate',
+        ['linear'],
+        ['band', 1],
+        0, [0,0,0,0],
+        1, [191, 33, 47, 1],
+        2, [249, 167, 62, 1],
+        3, [0, 111, 60, 1],
+        4, [38,75,150, 1]
+       ]
+
+    }
+  },
+  {
+    name: "Mud Only",
+    sources: [
+      {
+        min: 1,
+        max: 4,
+        nodata: 127,
+      }
+    ],
+    style: {
+      color: [
+        'interpolate',
+        ['linear'],
+        ['band', 1],
+        0, [0,0,0,0],
+        1, [191, 33, 47, 1],
+        2, [249, 167, 62, 0],
+        3, [0, 111, 60, 0],
+        4, [38,75,150, 0]
+       ]
+
+    }
+  }
+];
+
+// function createLayer(base, visualization) {
+//   const source = new GeoTIFF({
+//     normalize: false,
+//     min: 1,
+//     max: 4,
+//     nodata: 127,
+//     sources: visualization.sources.map((id) => ({
+//            url: 'https://tile-service-raster.s3.us-east-1.amazonaws.com/cogs/seafloor/HS51_Seafloor_Classification_webmer_cog.tif',
+//     })),
+//   });
+
+//   return new TileLayer({
+//     source: source,
+//     crossOrigin: 'anonymous',
+//     opacity: 0.35,
+//     saturation: 1,
+//     exposure: 0.25,
+//     contrast: 0.15,
+//     style: visualization.style,
+//   });
+// }
+
 // cog file load and colour values
 const cog = new TileLayer({
   visible: false,
   crossOrigin: 'anonymous',
   opacity: 0.35,
-  source: source,
+  source: cogSource,
   style: {
     color: [
       'interpolate',
@@ -75,8 +146,8 @@ const cog = new TileLayer({
   }
 })
 
-// Set base raster tiles 
-const baseRasterTile =  new MapLibreLayer({  
+// Set base raster tiles
+const baseRasterTile =  new MapLibreLayer({
   crossOrigin: 'anonymous',
   maplibreOptions: {
     style: './style/rasterTiles.json',
@@ -84,7 +155,7 @@ const baseRasterTile =  new MapLibreLayer({
 });
 
 // Set vector tiles
-const vectorTile = new MapLibreLayer({  
+const vectorTile = new MapLibreLayer({
   opacity: 0.75,
   crossOrigin: 'anonymous',
   maplibreOptions: {
@@ -95,7 +166,7 @@ const vectorTile = new MapLibreLayer({
 // draw map
 const map = new Map ({
   layers: [baseRasterTile, vectorTile, cog],
-  overlays: [overlay],
+  //overlays: [overlay],
   target: 'map',
   view: new View({
     center: olProj.fromLonLat([174.2,-41.18]),
@@ -105,21 +176,61 @@ const map = new Map ({
   })
 });
 
-// Set onclick to return values from COG
-map.on('singleclick', function(evt) {
-  const coordinate = evt.coordinate;
-  const data = cog.getData(evt.pixel);
-  console.log(data[0])
-  if (data[0]==1) {
-    var codeText = "Low reflectivity (mud)"
-  } else if (data[0]==2) {
-    var codeText = "Low - medium reflectivity (fine sand)"
-  } else if (data[0]==3) {
-    var codeText = "Medium - high reflectivity (medium sand)"
-  } else if (data[0]==4) {
-    var codeText = "High reflectivity (coarse sand, gravel)"
-  }
-  content.innerHTML = '<p>Seafloor classification value:</p><p><code>' + data[0] + '</code></p><p>' + codeText + '</p>';
-  overlay.setPosition(coordinate);    
-})
+// //toggle seafloor view
+// document.getElementById("menu-ui").onclick = function() {
+//   cog.setVisible(!cog.getVisible());
+// };
+
+const navSelect = document.getElementById('menu-ui');
+cogVis.forEach((visualization) => {
+  const option = document.createElement('option');
+  option.textContent = visualization.name;
+  navSelect.appendChild(option);
+});
+
+console.log(navSelect)
+
+// document.getElementById("menu-ui").onclick = function() {
+//   cog.setVisible(!cog.getVisible());
+// };
+
+
+// const visualizationSelector = document.getElementById('visualization');
+// cogVis.forEach((visualization) => {
+//   const option = document.createElement('option');
+//   option.textContent = visualization.name;
+//   visualizationSelector.appendChild(option);
+// });
+
+// function updateVisualization() {
+//   const visualization = cogVis[visualizationSelector.selectedIndex];
+//   const base =
+//     'https://tile-service-raster.s3.us-east-1.amazonaws.com/cogs/seafloor/HS51_Seafloor_Classification_webmer_cog.tif';
+
+//   const layer = createLayer(base, visualization);
+//   map.setLayers([layer]);
+
+//   map.setView(layer.getSource().getView());
+// }
+
+// visualizationSelector.addEventListener('change', updateVisualization);
+// updateVisualization();
+
+// // Set onclick to return values from COG
+// map.on('singleclick', function(evt) {
+//   const coordinate = evt.coordinate;
+//   const data = cog.getData(evt.pixel);
+//   console.log(data[0])
+//   if (data[0]==1) {
+//     var codeText = "Low reflectivity (mud)"
+//   } else if (data[0]==2) {
+//     var codeText = "Low - medium reflectivity (fine sand)"
+//   } else if (data[0]==3) {
+//     var codeText = "Medium - high reflectivity (medium sand)"
+//   } else if (data[0]==4) {
+//     var codeText = "High reflectivity (coarse sand, gravel)"
+//   }
+//   content.innerHTML = '<p>Seafloor classification value:</p><p><code>' + data[0] + '</code></p><p>' + codeText + '</p>';
+//   overlay.setPosition(coordinate);
+// })
 
